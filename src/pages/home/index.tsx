c9beae0006a7815042d11122d892178975344291
rjunio98 <robledorjunior1@gmail.com>
2023-10-24 10:14:02 -0300

    consumindo api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
 import { BsCartPlus } from "react-icons/bs";
 
+import { api } from "../../services/api";
+
+interface ProductProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
+
 export function Home() {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    getProducts();
+  }, []);
+
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
@@ -9,24 +31,29 @@ export function Home() {
         </h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
-            <img
-              className="w-full rounded-lg max-h-70 mb-2"
-              src=""
-              alt="Logo do produto"
-            />
-            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
+          {products.map((product) => (
+            <section key={product.id} className="w-full">
+              <img
+                className="w-full rounded-lg max-h-70 mb-2"
+                src={product.cover}
+                alt={product.title}
+              />
+              <p className="font-medium mt-1 mb-2">{product.title}</p>
 
-            <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90">R$ 1000</strong>
-              <button className="bg-zinc-900 p-1 rounded">
-                <BsCartPlus
-                  size={20}
-                  color="#fff"
-                />
-              </button>
-            </div>
-          </section>
+              <div className="flex gap-3 items-center">
+                <strong className="text-zinc-700/90">{product.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL"
+                })}</strong>
+                <button className="bg-zinc-900 p-1 rounded">
+                  <BsCartPlus
+                    size={20}
+                    color="#fff"
+                  />
+                </button>
+              </div>
+            </section>
+          ))}
         </div>
       </main>
     </div>
