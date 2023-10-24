ebb0523c4528d48413b8515f08327763dbfbcdeb
rjunio98 <robledorjunior1@gmail.com>
2023-10-24 09:22:33 -0300

    create page home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,34 @@
+import { BsCartPlus } from "react-icons/bs";
+
 export function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <main className="w-full max-w-7xl px-4 mx-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Produtos em alta
+        </h1>
+
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+          <section className="w-full">
+            <img
+              className="w-full rounded-lg max-h-70 mb-2"
+              src=""
+              alt="Logo do produto"
+            />
+            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
+
+            <div className="flex gap-3 items-center">
+              <strong className="text-zinc-700/90">R$ 1000</strong>
+              <button className="bg-zinc-900 p-1 rounded">
+                <BsCartPlus
+                  size={20}
+                  color="#fff"
+                />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
