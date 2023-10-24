47d94591f94b9b331f262a353e054bae7e6ae4aa
rjunio98 <robledorjunior1@gmail.com>
2023-10-24 09:31:53 -0300

    create page carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,33 @@
 export function Cart() {
   return (
-    <div>
-      <h1>Carrinho de compras</h1>
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+
+      <section className="flex items-center justify-between border-b-2 border-gray-300">
+        <img
+          className="w-28"
+          src=""
+          alt="Logo produto"
+        />
+
+        <strong>Preço: R$ 1.000</strong>
+
+        <div className="flex items-center justify-center gap-3">
+          <button className="bg-slate-600 px-2 text-white font-medium flex items-center justify-center">
+            -
+          </button>
+
+          2
+
+          <button className="bg-slate-600 px-2 text-white font-medium flex items-center justify-center">
+            +
+          </button>
+        </div>
+
+        <strong className="float-right">SubTotal: 1000</strong>
+      </section>
+
+      <p className="font-bold mt-4">Total: R$ 1.000</p>
     </div>
   );
 }
