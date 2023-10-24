48887013c975da15d6fb138ee7bc0cf7c2422980
rjunio98 <robledorjunior1@gmail.com>
2023-10-24 13:56:56 -0300

    adicionando produtos

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,8 +1,11 @@
 import { createContext, ReactNode, useState } from "react";
 
+import { ProductProps } from "../pages/home";
+
 interface CartContextData {
   cart: CartProps[];
   cartAmount: number;
+  addItemCart: (newItem: ProductProps) => void;
 }
 
 interface CartProps {
@@ -24,8 +27,33 @@ export const CartContext = createContext({} as CartContextData);
 function CartProvider({ children }: CartProviderProps) {
   const [cart, setCart] = useState<CartProps[]>([]);
 
+  function addItemCart(newItem: ProductProps) {
+    const indexItem = cart.findIndex((item) => item.id === newItem.id);
+
+    if (indexItem !== -1) {
+      let cartList = cart;
+
+      cartList[indexItem].amount = cartList[indexItem].amount + 1;
+      cartList[indexItem].total =
+        cartList[indexItem].amount * cartList[indexItem].price;
+
+      setCart(cartList);
+      return;
+    }
+
+    let data = {
+      ...newItem,
+      amount: 1,
+      total: newItem.price,
+    };
+
+    setCart((products) => [...products, data]);
+  }
+
   return (
-    <CartContext.Provider value={{ cart, cartAmount: cart.length }}>
+    <CartContext.Provider
+      value={{ cart, cartAmount: cart.length, addItemCart }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { BsCartPlus } from "react-icons/bs";
 
 import { api } from "../../services/api";
 
-interface ProductProps {
+import { CartContext } from "../../contexts/CartContext";
+
+export interface ProductProps {
   id: number;
   title: string;
   description: string;
@@ -12,6 +14,7 @@ interface ProductProps {
 }
 
 export function Home() {
+  const { addItemCart } = useContext(CartContext);
   const [products, setProducts] = useState<ProductProps[]>([]);
 
   useEffect(() => {
@@ -23,6 +26,10 @@ export function Home() {
     getProducts();
   }, []);
 
+  function handleAddCartItem(product: ProductProps) {
+    addItemCart(product);
+  }
+
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
@@ -32,7 +39,10 @@ export function Home() {
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
           {products.map((product) => (
-            <section key={product.id} className="w-full">
+            <section
+              key={product.id}
+              className="w-full"
+            >
               <img
                 className="w-full rounded-lg max-h-70 mb-2"
                 src={product.cover}
@@ -41,11 +51,16 @@ export function Home() {
               <p className="font-medium mt-1 mb-2">{product.title}</p>
 
               <div className="flex gap-3 items-center">
-                <strong className="text-zinc-700/90">{product.price.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL"
-                })}</strong>
-                <button className="bg-zinc-900 p-1 rounded">
+                <strong className="text-zinc-700/90">
+                  {product.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </strong>
+                <button
+                  className="bg-zinc-900 p-1 rounded"
+                  onClick={() => handleAddCartItem(product)}
+                >
                   <BsCartPlus
                     size={20}
                     color="#fff"
